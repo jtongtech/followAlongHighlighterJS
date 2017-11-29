04dbe9c34d911172cbd744a74e7915d3b24e8bef
JTong <eldertong@gmail.com>
2017-11-29 10:44:20 -0500

    completed 22 js

---
+++ js.js
@@ -0,0 +1,22 @@
+var triggers = document.querySelectorAll('a');
+var highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.appendChild(highlight);
+
+function highlightLink() {
+    var linkCoords = this.getBoundingClientRect();
+
+    var coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+    };
+
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+    console.log(coords);
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
